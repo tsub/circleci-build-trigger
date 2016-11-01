53a42afcfbc240d68a294d00023b6d855258b35b
tsub <tsubasatakayama511@gmail.com>
2016-11-02 00:11:56 +0900

    Create trigger function

--- handler.js
+++ handler.js
@@ -1,13 +1,22 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+require('dotenv').config();
+const axios = require('axios');
 
-  callback(null, response);
+axios.defaults.baseURL = 'https://circleci.com/api/v1'
+
+module.exports.trigger = (event, context, callback) => {
+  const circleToken = process.env.CIRCLE_TOKEN;
+  const project = process.env.PROJECT;
+  const branch = process.env.BRANCH;
+  const triggerBuildUrl = `/project/${project}/tree/${branch}?circle-token=${circleToken}`;
+  const triggerName = process.env.TRIGGER_NAME;
+  const buildParameters = {};
+  buildParameters[triggerName] = 'true';
+
+  axios.post(triggerBuildUrl, {
+    build_parameters: buildParameters
+  })
+  .then(response => { callback(null, 'Success'); })
+  .catch(error => { callback(error, error.stack); })
 };
